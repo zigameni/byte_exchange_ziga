5b7257decd3a9124162c0bb58255e00c37e43c9a
Ziga <shehu.meni@gmail.com>
2025-02-19 01:30:12 +0100

    For deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // eslint-disable camelcase
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
-// eslint-disable-next-line camelcase
+
 import { Inter, Space_Grotesk } from 'next/font/google'
 import type { Metadata } from 'next'; // for typescripting to know we are working with metadata
 
