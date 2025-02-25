055c3e08c9bb15a3af8d294685ab6d66bf1cb8eb
Ziga X <shehu.meni@gmail.com>
2025-02-25 22:32:35 +0100

    Doing stuff

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
+
     <main className="flex min-h-screen w-full items-center justify-center">
-      {children}
+  {children}
     </main>
   )
 }
--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 import Question from "@/components/forms/Question";
-import {auth} from "@clerk/nextjs";
-import {redirect} from "next/navigation";
-import {getUserById} from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getUserById } from "@/lib/actions/user.action";
 
 const Page = async () => {
+  const { userId } = auth();
+  // const userId = "user_dummyClerkId123";
+  // This is wierd
+  if (!userId) redirect("/sign-in");
+  //const [first, setfirst] = useState(second)
+  const mongoUser = await getUserById({ userId });
 
-    const {userId} = auth();
-    // const userId = "user_dummyClerkId123";
+  //console.log(mongoUser);
 
-    if (!userId) redirect('/sign-in');
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
+};
 
-    const mongoUser = await getUserById({userId});
+export default Page;
 
-    console.log(mongoUser);
-
-    return (
-        <div>
-            <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-            <div className="mt-9">
-                <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
-            </div>
-        </div>
-    )
-}
-
-export default Page
